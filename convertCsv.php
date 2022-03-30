99aa62eb484a0de2b454e5e3b0ad44ababd0fb2b
D4GNIR <jeremiecette@gmail.com>
2022-03-30 12:59:06 +0200

    init + work on csv

---
+++ convertCsv.php
@@ -0,0 +1,82 @@
+<div class="">
+    <form method="post" enctype="multipart/form-data"> 
+    
+    <div>
+        <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />  <!-- 10 Mo -->  
+        <input type="file" name="csv" required/>  
+    </div>
+    <div class="form-example">
+        <input type="submit" value="Envoyer!">
+    </div>
+    </form>
+</div>
+
+<?PHP
+// Répertoire pour les csv
+$uploads_dir = 'uploads';
+// Si il n'y a pas de dossier uploads il est créer
+if (!is_dir($uploads_dir)) {
+    mkdir($uploads_dir, 0777, true);
+}
+
+if (isset($_FILES['csv'])){                             //Test isset
+    if (!empty($_FILES['csv']['name'])){                //Test vide
+        if (($_FILES['csv']['type'] == 'text/csv' )){   //Test si c'est bien un CSV
+            // Je recupere tmp_name
+            $tmp_name = $_FILES["csv"]["tmp_name"];
+            // Je recupere le nom de $_FILES['csv']
+            $name = basename($_FILES["csv"]["name"]);
+            // ******************* Test si le fichier existe déjà ***********************
+            if (file_exists("$uploads_dir/$name")) {
+                // Je recupere l'extension de mon fichier
+                $ext = pathinfo($name, PATHINFO_EXTENSION);
+                // J'explose $name
+                $nameArray = explode('.',$name);
+                // J'utilise la premiere partie de $name
+                $name = $nameArray[0];
+                // Je récupere la date à cet instant
+                $date = dateNow();
+                // Je renomme mon fichier avec ma fonction date aleatoire
+                $name = $name."_".$date.".".$ext;
+                // Je déplace la csv dans mon dossier
+                $move = move_uploaded_file($tmp_name, "$uploads_dir/$name");
+            } else {
+                // Je déplace la csv dans mon dossier
+                $move = move_uploaded_file($tmp_name, "$uploads_dir/$name");
+            }
+            // ****************** /Test si le fichier existe déjà ***********************
+            // ************************* Traitement du CSV *****************************
+            $cvs_path = "$uploads_dir/$name";
+            // $tab = [];
+            // // Test l'existence du fichier et l'ouvre en lecture seule s'il existe.
+            // if(($handle = @fopen($cvs_path, "r")) !== false)// Le @ sert à ne pas afficher le E_WARNING relatif à une ouverture de fichier qui rate.
+            // {
+            // // Passe en revue chaque ligne du fichier CSV et la stocke dans $data.
+            // while(($datas = fgetcsv($handle, 0, ";")) !== false){
+            //      echo nl2br(print_r($datas, true));
+            //     }
+            // }
+            $file = fopen($cvs_path,"r");
+            print_r(fgetcsv($file));
+            fclose($file);
+
+
+
+
+
+        }else{
+            echo '<p>Le fichier doit être au format csv.</p>';
+        }
+    }else{
+        echo '<p>Il n\'y a rien a envoyé</p>';
+    } 
+}
+/********************************************************************
+***************************  Fonctions  *****************************
+*********************************************************************/
+//   Fonction pour renvoyer une date
+function dateNow(){
+    $now = date("m-d-y-His");
+    return $now;
+}
+
