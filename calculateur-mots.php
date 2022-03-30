241641b4485610258a777b4e63d0fba56d4aeb8b
Amandine Regad <90288439+regazzoniam@users.noreply.github.com>
2022-03-30 12:07:28 +0200

    Update Calculateur de mots - ARegad

--- calculateur-mots.php
+++ calculateur-mots.php
@@ -8,13 +8,13 @@ echo '
 </form>
 ';
 
-function calculateurMots($chaineCaractere) {//on définit la fonction qui calcule le nombre de mots
+function calculateurMots(htmlentities($chaineCaractere)) {//on définit la fonction qui calcule le nombre de mots
     $numberOfWords = str_word_count($chaineCaractere); //on définit la variable qui contient le nombre de mots dans la phrase renseignée par l'utilisateur
     return 'Il y a '.$numberOfWords.' mots dans la phrase "'.$chaineCaractere.'"'; //on retourne une phrase avec le nombre de mots et la phrase d'origine
 }
 
 if (isset($_POST['string']) && !empty($_POST['string'])) {//on vérifie qu'une phrase a bien été renseignée
-    echo calculateurMots(htmlentities($_POST['string']));//on affiche le retour de notre fonction
+    echo calculateurMots($_POST['string']);//on affiche le retour de notre fonction
 }else{
     echo 'Renseignez une phrase';//on affiche ceci dans le cas ou l'utilisateur arrive juste sur le site ou qu'il n'a pas renseigné de phrase
 }
