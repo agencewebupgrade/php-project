7857b63edbf0c88aef9c712f142f3578302584fa
Nicolas MEUNIER <n.meunier836@gmail.com>
2022-03-30 11:29:54 +0200

    Modification de la fonction Calculateur de mots

--- calculateur-mots.php
+++ calculateur-mots.php
@@ -1,8 +1,8 @@
 <?php
 
 function calculateurMots($chaineCaractere) {
-    echo str_word_count($chaineCaractere); 
+    return str_word_count($chaineCaractere); 
 }
 
 $str = 'Welcome to the jungle';
-calculateurMots($str);
+echo calculateurMots($str);
